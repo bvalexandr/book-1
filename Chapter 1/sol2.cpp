501c4db67b6e9c472248b84371f29964b4849ae8
bvalexandr <firebird-inferno@mail.ru>
2024-10-19 09:17:57 +0300

    Задание 2

---
+++ Chapter 1/sol2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+int main(void) {
+    std::cout << std::setw(4) << "1990" << std::setw(10) << "135" << std::endl;
+    std::cout << std::setw(4) << "1991" << std::setw(10) << "7290" << std::endl;
+    std::cout << std::setw(4) << "1992" << std::setw(10) << "11300" << std::endl;
+    std::cout << std::setw(4) << "1993" << std::setw(10) << "16200" << std::endl;
+    return 0;
+}
