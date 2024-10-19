feaa80a8b5fafd47a97350f69ab93afa57a682c3
bvalexandr <firebird-inferno@mail.ru>
2024-10-19 09:18:45 +0300

    Задание 7

---
+++ Chapter 1/sol7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void) {
+    double celsius_temp = 0.0;
+    std::cout << "Введите температуру в градусах Цельсия: ";
+    std::cin >> celsius_temp;
+    std::cout << "Температура в градусах Фаренгейта: " << celsius_temp * 9/5 + 32 << std::endl;
+    return 0;
+}
