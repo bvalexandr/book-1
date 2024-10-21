29697305b716bcdc529678b9c0369c78e147d9a3
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 09:38:59 +0300

    Задание 17

---
+++ Chapter 2/sol17.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+    int rows = 20;
+    int col = 1;
+    char fill_ch = 'X';
+    for (int row = 0; row < rows; row++) {
+        cout << setw(rows - col) << "";
+        cout << setfill(fill_ch);
+        cout << setw(1 + row*2) << "";
+        cout << setfill(' ');
+        cout << setw(rows - col) << "";
+        cout << '\n';
+        col++;
+    }
+    return 0;
+}
