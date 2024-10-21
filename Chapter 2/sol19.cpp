0cf5315de4563f1c7a6d866f14a4add685bf7699
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 09:54:56 +0300

    Задание 19

---
+++ Chapter 2/sol19.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    double first = 0.0;
+    int years = 0;
+    double percent = 0.0;
+    cout << "Введите начальный вклад: ";
+    cin >> first;
+    cout << "Введите число лет: ";
+    cin >> years;
+    cout << "Введите прочентную ставку: ";
+    cin >> percent;
+    percent /=100;
+    for (int i = 0; i < years; i++) {
+        first += first*percent;
+    }
+    cout << "Через " << years << " лет вы получите " << first << " долларов" << '\n';
+    return 0;
+}
