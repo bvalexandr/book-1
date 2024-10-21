832e1e407e8276be4d923f0554ed2329bae23f4b
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 08:51:16 +0300

    Задание 14

---
+++ Chapter 2/sol14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    char key = 0;
+    double temp = 0;
+    cout << "1. Перевод из градусов Цельсия в градусы по Фаренгейту\n";
+    cout << "2. Перевод из градусов по Фаренгейта в градусы Цельсия\n";
+    cin >> key;
+    switch (key) {
+        case ('1'):
+            cout << "Введите температуру в градусах Цельсия: ";
+            cin >> temp;
+            cout << "Температура по Фаренгейту: " << temp * 9 / 5 + 32 << '\n';
+            break;
+        case ('2'):
+            cout << "Введите температуру в градусах Фаренгейта: ";
+            cin >> temp;
+            cout << "Температура по Цельсию: " << (temp - 32) * 5/9 << '\n';
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
