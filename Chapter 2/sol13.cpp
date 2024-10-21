2e6508ffa46dac2b20657b944552082026d1753e
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 08:28:16 +0300

    Задание 13

---
+++ Chapter 2/sol13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+    int num = 0;
+    int rows = 20;
+    int cols = 10;
+    cout << "Введите целое число: ";
+    cin >> num;
+    for (int row = 1; row < rows+1; row++) {
+        for (int col = 1; col < cols+1; col++) {
+            cout << setw(8) << num*row*col << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
