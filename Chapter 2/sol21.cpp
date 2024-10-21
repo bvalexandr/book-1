66cb5ee1c2841690743b19aba45709771ad647c4
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 10:16:12 +0300

    Задание 21

---
+++ Chapter 2/sol21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int seat_count = 0;
+    int guest_count = 0;
+    long long int res = 1;
+    cout << "Введите количество гостей: ";
+    cin >> guest_count;
+    cout << "Введите количество стульев: ";
+    cin >> seat_count;
+    for (int i = 0; i < seat_count; i++) {
+        res *= (guest_count - i);
+    }
+    cout << guest_count << " гостей могут разместиться на " << seat_count << " стульях " << res << " способами\n";
+    return 0;
+}
