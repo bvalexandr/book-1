0f77707800f6886541b5686018e47942976c30bd
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 08:55:45 +0300

    Задание 15

---
+++ Chapter 2/sol15.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(void) {
+    long int res = 0;
+    char key = 0;
+    std::cout << "Введите число: ";
+    while ((key = getchar()) != '\n') {
+        res = res*10 + (key - '0');
+    }
+    std::cout << "Введено число: " << res << '\n';
+    return 0;
+}
