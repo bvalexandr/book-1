baddc8245a7e6daa20711a722f6fd2e5e1567173
bvalexandr <firebird-inferno@mail.ru>
2024-10-21 10:26:08 +0300

    Задание 22

---
+++ Chapter 2/sol22.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    double first = 0.0;
+    double first_copy = 0.0;
+    double after = 0;
+    double percent = 0.0;
+    int years = 0;
+    cout << "Введите начальный вклад: ";
+    cin >> first;
+    first_copy = first;
+    cout << "Введите целевую сумму вклада: ";
+    cin >> after;
+    cout << "Введите процентную ставку: ";
+    cin >> percent;
+    percent /=100;
+    first_copy += first_copy*percent;
+    while (first_copy <= after) {
+        years++;
+        first_copy += first_copy*percent;
+    }
+    cout << "Для достижения целевой суммы " << after << " при начальном вкладе " << first << " и процентной ставке " << percent*100 << " потребуется " << years << " лет. При этом вы получите " << first_copy << " долларов\n";
+    return 0;
+}
